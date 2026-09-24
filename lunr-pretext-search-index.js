f28781c6619adfa667e8434e4503550b4ce60f7a
frisbie4296 <salmonra@msu.edu>
2026-09-24 19:10:14 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,140 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-chapter-title-navigation",
+  "level": "1",
+  "url": "sec-chapter-title-navigation.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "How to Navigate This Document",
+  "body": " How to Navigate This Document    "
+},
+{
+  "id": "sec-chapter-title-learning-outcomes",
+  "level": "1",
+  "url": "sec-chapter-title-learning-outcomes.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Learning Outcomes",
+  "body": " Learning Outcomes  In this project, you will research the contributions of STEM professionals to the Holocaust. These professionals may be individuals or they may be organizations. At the end of this project, you will prepare and present a poster that:    Briefly tells the story of your selected professional and how they contributed to the Holocaust;  Includes descriptions of the science or other professional knowledge that enabled your selected professional to contribute to the Holocaust;  Identifies which provisions, if any, of modern professional ethical guidelines were violated by your selected professional;  Reflects on what your professional's story tells us about what it means to be an ethical scientist; and  Is written appropriately for a general audience.    The specific learning outcomes for this project are:    Describe how a STEM professional or organization contributed to the Holocaust.  Integrate domain knowledge, mathematical\/data science content, and ethics within the context of the Holocaust.  Articulate what it means to be an ethical STEM professional.  Reflect on how ethics will inform future STEM professional practice.  Design effective communication about ethical STEM practice for a general audience.    This project will involve a trip to the Zekelman Holocaust Center in Farmington Hills. Your trip and food will be paid for.  Please be sure to specifically focus on your selected professional's contribution to the Holocaust, not to the German war effort. In other words, look for how your professional’s knowledge and skills helped enable the systematic killing of 6 million European Jews along with millions of other marginalized individuals.  We will occasionally invite you to complete surveys and participate in interviews in order for us to examine how well this project achieved these learning outcomes.  "
+},
+{
+  "id": "sec-module-1-intro",
+  "level": "1",
+  "url": "sec-module-1-intro.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Introduction",
+  "body": " Introduction  This section is about framing the choice of STEM professional - should be someone at least adjacent to your major or area of interest.  "
+},
+{
+  "id": "sec-module-1-resources",
+  "level": "1",
+  "url": "sec-module-1-resources.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Resources",
+  "body": " Resources   STEM Professionals     Actuarial Science (by way of Insurance Companies)    Allianz  Munich RE  Kurt Schmitt      Biology, Pharmacology, and Health    IG Farben  Joseph Mengele  Karl Brandt  Ernst Wentzler  Irmfried Eberl  Emily Kraeplin  Joachim Mrugowsky  Waldemar Hoven  Carl Schneider  Julius Hallervorden  Helmut Vetter  Bruno Weber  Victor Capesius      Chemistry    I.G. Farben  Dehesch (subsidiary of I.G. Farben)  Tesch (subsidiary of I.G. Farben)  Bruno Tesch  Karl Weinbacher  August Becker  Albert Widmann      Data\/Computer Science\/Statistics    IBM  Francis Galton  Karl Pearson  Ronald Fischer  Charles Davenport (also a biologist)      Engineering    IG Farben  IBM  Topf and Sons  Hams Kammler  Kurt Prüfer  Fritz Sander  Karl Schultze  Karl Bischoff        "
+},
+{
+  "id": "sec-module-holocaust-museum-visit-norms",
+  "level": "1",
+  "url": "sec-module-holocaust-museum-visit-norms.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Discussion Norms",
+  "body": " Discussion Norms    Take care of your well-being and step out if needed   Share the air    Monitor how much you're participating\/how much space you're occupying  Avoid interrupting folks      Seek first to understand, then to be understood    Revoice what folks say or ask for clarifying questions before responding  Practice active listening      Speak for yourself, not others    Avoid generalizations  Respect others' experiences     Consider whose viewpoints are being excluded   Use both\/and rather than either\/or thinking    “Yes, and” language     Be aware of, and work to deconstruct, the existing power dynamics in the room  Presume positive intentions    "
+},
+{
+  "id": "sec-module-intro-prompts",
+  "level": "1",
+  "url": "sec-module-intro-prompts.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Introduction and Reflection",
+  "body": " Introduction and Reflection  In your groups, discuss the following:    Share your names and introduce yourselves to one another.   Take 5 minutes to write down your thoughts reflecting on the tour. Think about:    What stuck out to you?  What do you think will stay with you?  How has it shaped your understanding of the work your individual\/organization did?     Now take ~10 minutes to share out some of your reflections with your peers (just 1-2 minutes per student).   Share the individual or organization you are studying:    Why you chose that individual\/organization  What you have found so far       "
+},
+{
+  "id": "sec-module-ethical-codes",
+  "level": "1",
+  "url": "sec-module-ethical-codes.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Modern Professional Ethical Codes",
+  "body": " Modern Professional Ethical Codes  Now we will turn to ethics. Your individual or organization's contributions were in some area of STEM and today, STEM professionals are bound by ethical guidelines and codes. Review the appropriate guidelines for the STEM area you have chosen.     Actuarial Science    SOA Code of Professional Conduct  Casualty Actuarial Society Code of Professional Conduct      Biology, Pharmacology, and Health    American Medical Association Code of Medical Ethics (by Chapter)  American Pharmacists Association Code of Ethics for Pharmacists  American Psychological Association Ethical Principles of Psychologists and Code of Conduct      Chemistry    American Chemical Society Chemical Professional's Code of Conduct (intended for domestic US members)  American Chemical Society Global Chemist's Code of Ethics (intended for international members)      Computer Science    Association for Computing Machinery Code of Ethics      Engineering    American Society of Civil Engineers Code of Ethics  American Society of Mechanical Engineers Code of Ethics  National Society of Professional Engineers Code of Ethics for Engineers      Statistics    American Statistical Association Ethical Guidelines for Statistical Practice       "
+},
+{
+  "id": "sec-module-ethical-codes-discussion",
+  "level": "1",
+  "url": "sec-module-ethical-codes-discussion.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Ethical Codes Discussion",
+  "body": " Ethical Codes Discussion  In your groups, discuss the following:   What guidelines did your individual or organization violate?    Were the activities a clear violation or do they depend on some interpretation?  When were the guidelines established in history?  Consider that these guidelines are updated regularly - how far back do the guidelines go?      How are the ethical guidelines you identified enforced?    What responsibilities (if any) do others in the profession have to report one who violates the ethical guidelines?  Is there a formal governing body that oversees their enforcement? How is it structured?  What are the consequences of violating the ethical guidelines?      What language (if any) is there that suggests how the governing body in your profession views what it means to be an ethical scientist?    Do the ethical guidelines suggest an affirmative duty to good or merely an obligation to not do harm?  Do you agree with that perspective? Why or why not?       "
+},
+{
+  "id": "sec-module-ethical-lenses-discussion",
+  "level": "1",
+  "url": "sec-module-ethical-lenses-discussion.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Framework for Ethical Decision Making",
+  "body": " Framework for Ethical Decision Making  The Santa Clara Markkula Center for Applied Ethics has a website describing a Framework for Ethical Decision Making .  This includes a list of five ethical lenses. An ethical lens is how one thinks about and applies ethics in their decisions. Read through the list of lenses and discuss the following with your group (make sure to record your answers!):    Which len(es) do you identify strongest with? Why?  Which len(es) do you think embody the ethical guidelines you reviewed? Justify your answers.    "
+},
+{
+  "id": "sec-module-stem-professional-discussion",
+  "level": "1",
+  "url": "sec-module-stem-professional-discussion.html",
+  "type": "Section",
+  "number": "3.6",
+  "title": "STEM Professionals",
+  "body": " STEM Professionals  Science is powerful. Having studied how a STEM professional contributed to the Holocaust and viewed artifacts documenting the history of the Holocaust, you have seen some of the worst ways to use that power.  Having reviewed ethical guidelines for a STEM profession, you have seen current minimal expectations for how to use that power responsibly.  Take some time to think, and then write down, what you think being an Ethical STEM professional means to you and what this will look like for your conduct in the workplace.  Here are some prompts to get you started, but feel free to deviate from this list:    What have you learned about ethical guidelines in your profession and your perspective on ethical professional practice?  How did the tour of the Holocaust Center and your examination of the scientific contributions to the Holocaust illustrate the need for science ethics? How does that shape your view of ethical science practice?    "
+},
+{
+  "id": "sec-background-research-resources",
+  "level": "1",
+  "url": "sec-background-research-resources.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Resources",
+  "body": " Resources     Black, E. (2001). IBM and the Holocaust: The Strategic Alliance Between Nazi Germany and America's Most Powerful Corporation . New York: Crown Publishers.    Green, A. (2020). Nazi data science: The dark history that led to modern data laws . Blog. Link: Nazi Data Science    Katz, E. (2006). On the neutrality of technology: The Holocaust death camps as a counter-example. Journal of Genocide Research , vol. 7, iss. 3, pgs. 409-421. Link: tandfonline.com    Katz, E. (2011). The Nazi engineers: Reflections on technological ethics in hell. Science and Engineering Ethics , vol. 17, pgs. 571-582. Link: springer.com    Lopez-Munoz, F., Alamo, C., Garcia-Garcia, O., Molina, J.D., and Rubio, G. (2008). The role of psychopharmacology in medical abuses of the Third Reich: From euthanasia programmes to human experimentation. Brain Research Bulletin , vol. 77, iss. 6, pgs. 388-403. Link: sciencedirect.com    From Allianz' website: Allianz during the Nazi era (PDF)     "
+},
+{
+  "id": "ch-module-reflections-2",
+  "level": "1",
+  "url": "ch-module-reflections-2.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Instructions",
+  "body": " Instructions  The last part of this project is writing a small reflection (1 - 2 pages) about your experiences. We do not expect this document to be highly polished, we want to hear your perspectives and value hearing your individual voice!  There is no specific format or content you must include, however, below are a few ideas to get you started:    What was your favorite part of this honors experience? Why? What was your least favorite?  What was something you learned that surprised you the most?  How has this honors project changed the way you think about the Holocaust? What bout the way you think about science and ethics?  Will this change in perspective have a lasting effect on you? In what way?  Based on what you've learned, what do you think the general public should know about the Holocaust, STEM, and ethics?  How do you plan to practice ethical STEM now and in the future? Is your answer now different from what it would have been at the beginning of the semester?  What would you change about this experience? What would you keep the same?    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
